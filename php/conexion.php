fc3db14f8f56305685ffc8b9e96ada85599ac8ed
carlosgr55 <124015981+carlosgr55@users.noreply.github.com>
2024-11-29 17:57:13 -0600

    Ya se conecto!!!"

--- php/conexion.php
+++ php/conexion.php
@@ -1,23 +1,28 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$base = "clinica";
+$server = "localhost";
+$user = "root";
+$database = "clinica";
 $password = "Motomami55";
 
+$conexion = mysqli_connect($server, $user, $password, $database);
+if (!$conexion) {
+    die("" . mysqli_connect_error());
+} else {
+    echo "Me pude conectar lol";
+    $query = "SELECT curp, nombre FROM paciente";
 
-    $conexion = mysqli_connect($servidor, $usuario,$password, $base);
-    echo "Conexion establecida";
-
-    $sql = "SELECT curp, nombre FROM paciente";
-    $resultado = mysqli_query( $conexion, $sql);
-    $campos = $resultado -> fetch_fields();
-
-    foreach($campos as $val){
-        echo "CURP ".$val -> curp;
-        echo "Nombre ".$val -> nombre;
-
+    if ($sentencia = mysqli_prepare($conexion, $query)) {
+        mysqli_stmt_execute($sentencia);
+        mysqli_stmt_bind_result($sentencia, $curp, $nombre);
+        while (mysqli_stmt_fetch($sentencia)) {
+            echo "CURP ".$curp ." <br>Nombre: ".$nombre."";
+        }
+        mysqli_stmt_close($sentencia);
+    }else{
+        echo "Error en consulta ".mysqli_error($conexion);
     }
 
-    print_r($resultado);
+}
+
 
 ?>
